cc4b34ee0573c2f5be4b76bd3d0d4e40272fde96
AnalyticsInAction <Steve@analyse.us>
2024-03-03 20:45:00 +1300

    Connect_2_PBIX.py
    Connects to open PBIX and loads tables in semantic model into a dataframe for further processing

--- ConnectPBIX.py
+++ ConnectPBIX.py
@@ -1,8 +1,7 @@
 import adodbapi
+import pandas as pd
 
 # Connection string
-#easiest place to get the PORT (60668 in the example below) is from DAX Studio
-#similarly the Initial Catalog can be accessed from the connection page in DAX Studio under "advanced options"
 conn_str = "Provider=MSOLAP;Data Source=localhost:60668;Initial Catalog=d87ae86c-8953-4448-a0cd-dcce467ea175;"
 
 conn = adodbapi.connect(conn_str)
@@ -10,14 +9,36 @@ conn = adodbapi.connect(conn_str)
 # Create a cursor
 curs = conn.cursor()
 
-# Replace with your actual query
-sql = "EVALUATE 'SalesLT Product'"  # Example SQL query
-curs.execute(sql)
+# SQL to query the DMV for a list of table names
+sql_tables = "SELECT * FROM $SYSTEM.TMSCHEMA_TABLES"
 
-# Fetch and print rows
-rows = curs.fetchall()
-for row in rows:
-    print(row)
+curs.execute(sql_tables)
+
+# Fetch table names
+table_names = curs.fetchall()
+
+# Dictionary to hold each table DataFrame
+tables_df = {}
+
+# Iterate through each table name and load it into a DataFrame
+for row in table_names:
+    table_name = row[2]  # Assuming the table name is in the third column
+    print(f"Loading DataFrame for table: {table_name}")
+    sql = f"EVALUATE '{table_name}'"
+    curs.execute(sql)
+    rows = curs.fetchall()
+    # Assuming first row contains column headers
+    headers = [column[0] for column in curs.description]
+    df = pd.DataFrame(rows, columns=headers)
+    tables_df[table_name] = df
+    print(f"Created DataFrame: {table_name}")
+    #display(tables_df[table_name])
+
+    # Print the DataFrame explicitly
+    print(f"DataFrame '{table_name}' contents:")
+    #print(tables_df[table_name])
+    display(tables_df[table_name])
+    
 
 # Close the cursor and connection
 curs.close()
